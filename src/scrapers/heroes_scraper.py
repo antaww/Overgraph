69365f3ec5d614edb7c6c104f8011a392e59bc8e
antaww <anta.pizzetta@gmail.com>
2024-01-25 08:37:52 +0100

    feat(scraper): Get every roles of every seasons heroes stats.

--- src/scrapers/heroes_scraper.py
+++ src/scrapers/heroes_scraper.py
@@ -11,6 +11,9 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 hero_div = soup.find_all('tr', class_='group even:bg-surface-even odd:bg-surface-odd')
 heroes = {}
+max_season_ow2 = 8
+max_season_ow1 = 36
+skill_tiers = ['all', 'bronze', 'silver', 'gold', 'platinum', 'diamond', 'master', 'grandmaster']
 
 
 def handle_hero_name():
@@ -35,36 +38,42 @@ for i in range(0, len(hero_div), 1):
 for hero_name in heroes:
     print(hero_name)
     heroes[hero_name]['stats'] = {}
-    # overbuff bug :
-    # ow1 : 1/2/4/7/10/16 not working
-    # ow2 : 7 not working
-    max_season_ow2 = 8
-    max_season_ow1 = 36
-    for game in range(1, 2+1, 1):
-        for season_nbr in range(1, max_season_ow1+1, 1):
-            print(f'scraping {hero_name} in ow{game}s{season_nbr}')
-            if game == 2 and season_nbr > max_season_ow2:
-                print(f'Season {season_nbr} in ow{game} not available')
-                break
-            if season_nbr < 10:
-                season_nbr = f'0{season_nbr}'
-            season = f'ow{game}s{season_nbr}'
-            delay = 0.2
-            url = f'https://www.overbuff.com/heroes/{hero_name}?gameMode=competitive&season={season}'
-            response = requests.get(url, headers=headers)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            try:
-                seasons = {}
-                stats = {}
-                stat_value = soup.find_all('div', class_='text-sm sm:text-base leading-none')
-                stat_name = soup.find_all('div', class_='text-xs sm:text-sm text-secondary')
-                for i in range(0, len(stat_value), 1):
-                    stats[stat_name[i].text] = stat_value[i].text
-                    if stats[stat_name[i].text] == '—':
-                        stats[stat_name[i].text] = 'Unknown'
-                heroes[hero_name]['stats'][season] = stats
-            except AttributeError:
-                print(f'No stats for {hero_name} in {season}')
+
+    # loop through skill tiers
+    for skill_tier in skill_tiers:
+        heroes[hero_name]['stats'][skill_tier] = {}
+
+        # overbuff bug :
+        # ow1 : 1/2/4/7/10/16 not working
+        # ow2 : 7 not working
+        for game in range(1, 2 + 1, 1):
+            for season_nbr in range(1, max_season_ow1 + 1, 1):
+                print(f'scraping {hero_name} in ow{game}s{season_nbr} in {skill_tier}')
+                if game == 2 and season_nbr > max_season_ow2:
+                    print(f'Season {season_nbr} in ow{game} not available')
+                    break
+                if season_nbr < 10:
+                    season_nbr = f'0{season_nbr}'
+                season = f'ow{game}s{season_nbr}'
+                delay = 0.2
+                url = (f'https://www.overbuff.com/heroes/{hero_name}'
+                       f'?gameMode=competitive'
+                       f'&season={season}'
+                       f'&skillTier={skill_tier}')
+                response = requests.get(url, headers=headers)
+                soup = BeautifulSoup(response.text, 'html.parser')
+                try:
+                    seasons = {}
+                    stats = {}
+                    stat_value = soup.find_all('div', class_='text-sm sm:text-base leading-none')
+                    stat_name = soup.find_all('div', class_='text-xs sm:text-sm text-secondary')
+                    for i in range(0, len(stat_value), 1):
+                        stats[stat_name[i].text] = stat_value[i].text
+                        if stats[stat_name[i].text] == '—':
+                            stats[stat_name[i].text] = 'Unknown'
+                    heroes[hero_name]['stats'][skill_tier][season] = stats
+                except AttributeError:
+                    print(f'No stats for {hero_name} in {season}')
 
 with open('../results/heroes.json', 'w') as f:
     json.dump(heroes, f, indent=4)
