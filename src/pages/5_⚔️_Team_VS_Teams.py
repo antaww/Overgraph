4a0f2fcbec57105f4b87c447bb40943c015cb5cf
antaww <anta.pizzetta@gmail.com>
2024-02-09 03:16:10 +0100

    feat(streamlit): Team vs teams page infos + tabs.

--- src/pages/5_⚔️_Team_VS_Teams.py
+++ src/pages/5_⚔️_Team_VS_Teams.py
@@ -2,31 +2,50 @@ import os
 
 import streamlit as st
 
+st.set_page_config(
+    page_title="Overgraph - Team VS Teams",
+    page_icon="⚔️",
+)
+
+
+def display_page_infos():
+    st.subheader('Get team scores against other teams')
+    st.markdown("""
+                    The collected datas provide a comprehensive overview of one **team's performances against its Overwatch League opponents**. 
+                    The records indicate the total number of matches / rounds played, wins, losses and win percentage for each team matchup across multiple seasons. 
+                    For a high level team coach, it is important to analyze the team's performance against other teams to understand their strengths and weaknesses. 
+                    It could help inform strategic decision-making going forward. 
+                    """)
+
+
 try:
+    df_tab, viz_tab = st.tabs(["Dataframe", "Visualization"])
     map_stats = st.session_state.map_stats
     df = st.session_state.df
     get_team_scores = st.session_state.get_team_scores
 
-    st.set_page_config(
-        page_title="Overgraph - Team VS Teams",
-        page_icon="⚔️",
-    )
     teams_list = df['team'].unique()
     map_types_list = df['map_type'].str.title().unique()
 
-    st.subheader('Get team scores against other teams')
-    # create a dropdown list with every unique 'stat'
-    team = st.selectbox('Select a team', teams_list)
-    map_type = st.selectbox('Select a map (not required)', [''] + list(map_types_list))
-    st.markdown('_**Only Matches** column means : if the score is based on the whole match or only the map_')
-    # display the result
-    try:
-        st.write(get_team_scores(team, map_type))
-        st.subheader(f'Winrate of {team} against other teams on {map_type + ' maps' if map_type else "all types "
-                                                                                                     "of matches"}')
-        st.bar_chart(get_team_scores(team, map_type).set_index('Opponent')['Winrate'], color='#efcfd0')
-    except KeyError:
-        st.error('No data available for this team and this map type')
+    with df_tab:
+        display_page_infos()
+        # create a dropdown list with every unique 'stat'
+        team = st.selectbox('Select a team', teams_list)
+        map_type = st.selectbox('Select a map (not required)', [''] + list(map_types_list))
+        st.markdown('_**Only Matches** column means : if the score is based on the whole match or only the map_')
+        # display the result
+        try:
+            st.write(get_team_scores(team, map_type))
+        except KeyError:
+            st.error('No data available for this team and this map type')
+    with viz_tab:
+        display_page_infos()
+        st.subheader(
+            f'Winrate of {team} against other teams on {map_type + ' maps' if map_type else "all types of matches"}')
+        try:
+            st.bar_chart(get_team_scores(team, map_type).set_index('Opponent')['Winrate'], color='#efcfd0')
+        except KeyError:
+            st.error('No data available for this team and this map type')
 except AttributeError:
     # explain that the user goes to the page Heroes without having loaded the data from the Home page
     st.error('You need to load the data from the Home page first !')
