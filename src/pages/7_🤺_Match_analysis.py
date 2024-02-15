02160220ac6ca38926eac12a9c0a52d9c78e4c7d
Bahsiik <o.mist1303@gmail.com>
2024-02-16 00:11:28 +0100

    feat(streamlit): Add the heroes time played stat by match in the match analysis page.

--- src/pages/7_🤺_Match_analysis.py
+++ src/pages/7_🤺_Match_analysis.py
@@ -1,9 +1,9 @@
 import os
 
-import altair as alt
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 st.set_page_config(
     page_title="Overgraph - Match analysis",
@@ -25,6 +25,7 @@ try:
     # team_one_tab, team_two_tab = st.tabs(["Winner Team", "Looser Team"])
     map_stats = st.session_state.map_stats
     df = st.session_state.df
+    get_match_analysis_heroes_played = st.session_state.get_match_analysis_heroes_played
 
     teams_list = df['team'].unique()
     stats_list = df['stat'].unique()
@@ -53,7 +54,6 @@ try:
     match = st.selectbox('Select a match', match_list)
     match_id = match.split('(')[1].split(')')[0]
 
-
     maps = map_stats[map_stats['match_id'] == int(match_id)]['map_name'].unique()
 
     df_tab, viz_tab = st.tabs(["Dataframe", "Visualization"])
@@ -65,6 +65,40 @@ try:
         st.write(df[df['match_id'] == int(match_id)])
     with viz_tab:
         map_tabs = st.tabs([map_name for map_name in maps])
+
+        for i in range(len(maps)):
+            with map_tabs[i]:
+                heroes_played = get_match_analysis_heroes_played(stage, int(match_id), maps[i], all=False)
+                teams = heroes_played['Team'].unique()
+                figures = []
+                players_name = []
+                for team in teams:
+                    players = heroes_played[heroes_played['Team'] == team]['Player'].unique()
+                    for player in players:
+                        player_heroes = heroes_played[
+                            (heroes_played['Team'] == team) & (heroes_played['Player'] == player)]
+                        trace = go.Bar(x=player_heroes['Hero'], y=player_heroes['Time Played'], name=player)
+                        figures.append(trace)
+                        players_name.append(player)
+
+                total_players = len(players_name)
+                st.subheader(f'Time Played by heroes for each teams in {maps[i]}')
+
+                team_one_subplots = make_subplots(rows=2, cols=3, subplot_titles=players_name[:total_players // 2])
+                team_one_subplots.update_layout(title_text=f'Heroes played for each player of {teams[0]}', bargap=0.5)
+                team_two_subplots = make_subplots(rows=2, cols=3, subplot_titles=players_name[total_players // 2:])
+                team_two_subplots.update_layout(title_text=f'Heroes played for each player of {teams[1]}', bargap=0.5)
+
+                half_players = total_players // 2
+
+                for i in range(half_players):
+                    team_one_subplots.add_trace(figures[i], row=(i // 3) + 1, col=(i % 3) + 1)
+
+                for i in range(half_players, total_players):
+                    team_two_subplots.add_trace(figures[i], row=((i - half_players) // 3) + 1, col=((i - half_players) % 3) + 1)
+                st.plotly_chart(team_one_subplots, use_container_width=True)
+                st.plotly_chart(team_two_subplots, use_container_width=True)
+
 
 except AttributeError:
     # explain that the user goes to the page Heroes without having loaded the data from the Home page
